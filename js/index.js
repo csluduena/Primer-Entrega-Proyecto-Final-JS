ff61c158c6c0be5e433b1b9a5470bae21cef4397
Carlos Sebastian Ludueña <csluduena@gmail.com>
2024-02-22 23:44:44 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// Al cargar la página, se muestra un prompt que pide el nombre del usuario
+let user_Name = prompt("Por favor, introdusca su nombre:");
+
+// Si el usuario introduce un nombre, se guarda en la variable user_Name
+// Si el usuario cancela el prompt, la variable user_Name es null
+
+alert("Bienvenido " + user_Name + ' a "CarpiCoder te pone la nota"');
+
+alert("A continuación se le mostrará una lista de la comisión #53940 JAVASCRIPT, introduzca nombre y apellido para consultar la nota.");
+
